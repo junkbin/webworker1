45224aa616cd4afdd144d10bc4e87826cbac3c02
santosh <santosh.ece06@gmail.com>
2019-01-14 17:51:16 +0530

    test

--- src/web.worker.js
+++ src/web.worker.js
@@ -1,8 +1,17 @@
-self.addEventListener('message', (e)=>{
-    let sthis = this;
+let sampleFunction = (e)=>{
     console.log(e);
+    let sthis = this;
 
     setInterval(()=>{
         sthis.postMessage(new Date());
     }, 1000);
+}
+
+self.addEventListener('message', (e)=>{
+    let sthis = this;
+    console.log(e);
+
+    sampleFunction(e);
 }, false);
+
+
